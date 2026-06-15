b848abaa46b092eea9e5f5c2b5e710d72afdda28
Vinmatweb <vinmat.sn@gmail.com>
2026-06-15 16:56:30 +0200

    1. nahrani world for kids - verze 0
    nejjednodussi verze webu

---
+++ seznam.js
@@ -0,0 +1,26 @@
+// =========================================================================
+// SÉRIE 0: SEM POUZE PŘIPISUJ NÁZVY SOUBORŮ PODLE TOHO, KAM JSI JE NAHRÁL
+// Pokud má položka barevnou i černobílou verzi, BW variantu sem NEPIŠ.
+// Pokud je položka POUZE černobílá, musí název souboru končit na _BW.png/.jpg
+// =========================================================================
+// =========================================================================
+
+const dataBludiste = [
+    "LV2-Gem_1-Ant.png", //    ".png",
+    "LV2-Gem_2-Bee.png",
+    "LV2-GPT_2-Bunny.png",
+];
+
+const dataOmalovanky = [
+    "LV1-Gem_1-Bear_BW.png",
+    "LV2-Gem_1-Bear_BW.png",
+    "LV3-Gem_1-Bear_BW.png",
+    "LV4-Gem_1-Bear_BW.png",
+    "LV5-Gem_1-Bear_BW.png",
+    "LV1-Gem_2-baby in stroller_BW.png",
+    "LV2-Gem_2-baby in stroller_BW.png"
+];
+
+const dataSpojovacky = [
+    "LV1-Gem_1-Bear_BW.png"
+];
