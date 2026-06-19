8e246aa52047c34af9a6eb7affe99a84cbb3a855
Vinmatweb <vinmat.sn@gmail.com>
2026-06-19 11:31:54 +0200

    pridan slovnik

--- seznam.js
+++ seznam.js
@@ -1,14 +1,31 @@
 // =========================================================================
+// 🌐 ČESKÝ SLOVNÍK PRO PŘEKLAD NÁZVŮ OBRÁZKŮ
+// Sem jednoduše připisuj překlady pro nová zvířátka a objekty.
+// Formát je: 'anglicky_nazev_v_souboru': 'Český Název Na Webu'
+// =========================================================================
+const prekladySlov = {
+    'bear': 'Medvídek',
+    'ant': 'Mravenec',
+    'bee': 'Včelka',
+    'bunny': 'Zajíček',
+    'baby in stroller': 'Miminko v kočárku',
+    'car': 'Auto',
+    'rainbow': 'Duha',
+    'horse': 'Koník'
+
+    // SEM POHODLNĚ PŘIPISUJ DALŠÍ
+};
+
+// =========================================================================
 // SÉRIE 0: SEM POUZE PŘIPISUJ NÁZVY SOUBORŮ PODLE TOHO, KAM JSI JE NAHRÁL
 // Pokud má položka barevnou i černobílou verzi, BW variantu sem NEPIŠ.
 // Pokud je položka POUZE černobílá, musí název souboru končit na _BW.png/.jpg
 // =========================================================================
-// =========================================================================
 
 const dataBludiste = [
-    "LV2-Gem_1-Ant.png", //    ".png",
+    "LV2-Gem_1-Ant.png",
     "LV2-Gem_2-Bee.png",
-    "LV2-GPT_2-Bunny.png",
+    "LV2-GPT_2-Bunny.png"
 ];
 
 const dataOmalovanky = [
